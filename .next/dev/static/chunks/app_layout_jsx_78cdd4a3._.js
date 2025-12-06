e4d46ff87e3357a9e858a05b0635691af6789420
aliciafm42 <156551143+aliciafm42@users.noreply.github.com>
2025-12-06 17:51:17 -0500

    3

---
+++ .next/dev/static/chunks/app_layout_jsx_78cdd4a3._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_6e850112._.css",
+  "static/chunks/_d6621ed6._.js"
+],
+    source: "dynamic"
+});
